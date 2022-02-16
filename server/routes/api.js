02d4980e16a881b821dc9157ff2d70bd31be8254
aymanab5 <aymansce@gmail.com>
2022-02-16 13:15:48 +0200

    server side - every time get new 5 pics

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,16 +6,27 @@ const axios = require("axios");
 const NodeCache = require("node-cache");
 const myCache = new NodeCache({ stdTTL: 60 * 60 * 24 });
 
-const photosUrl = "https://picsum.photos/v2/list?page=1&limit=5";
+const photosUrl = "https://picsum.photos/v2/list?page=1&limit=100";
 
 router.get("/photos", async (req, res) => {
   try {
     if (myCache.has("photos")) {
-      res.send(myCache.get("photos")).status(200);
+      res
+        .send(
+          myCache
+            .get("photos")
+            .slice(myCache.get("page"), myCache.get("page") + 5)
+        )
+        .status(200);
+
+      myCache.set("page", myCache.get("page") + 5);
+      if (myCache.get("page") >= 95) myCache.set("page", 0);
     } else {
       const photos = await axios.get(photosUrl);
       myCache.set("photos", photos.data);
-      res.send(photos.data).status(200);
+      myCache.set("page", 0);
+      res.send(photos.data.slice(0, 5)).status(200);
+      myCache.set("page", myCache.get("page") + 5);
     }
   } catch (error) {
     res.send(error).status(400);
