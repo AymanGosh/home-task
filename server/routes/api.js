6ad924c2e7bc61c83ce85d64271323e731d61070
aymanab5 <aymansce@gmail.com>
2022-02-16 13:28:43 +0200

    Clean the get route in api

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,15 +11,12 @@ const photosUrl = "https://picsum.photos/v2/list?page=1&limit=100";
 router.get("/photos", async (req, res) => {
   try {
     if (myCache.has("photos")) {
+      const pageNumber = myCache.get("page");
       res
-        .send(
-          myCache
-            .get("photos")
-            .slice(myCache.get("page"), myCache.get("page") + 5)
-        )
+        .send(myCache.get("photos").slice(pageNumber, pageNumber + 5))
         .status(200);
 
-      myCache.set("page", myCache.get("page") + 5);
+      myCache.set("page", pageNumber + 5);
       if (myCache.get("page") >= 95) myCache.set("page", 0);
     } else {
       const photos = await axios.get(photosUrl);
